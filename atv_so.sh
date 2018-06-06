61352eb03e299ef3f071f72cf610c78f309556f1
Tiago Nunes <tiaguzaum_@hotmail.com>
2018-06-05 23:58:55 -0400

    Primeira versao
    Adicionado arquivo com as frases e o script da atividade.

---
+++ atv_so.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+date
+
+cat frases | grep $[1+$[RANDOM % 3]]
+echo "== Disco Rígido =="
+echo "Ocupado: " 
+df |grep /  | awk '{total += $3};END {total/=1048576} ;  END {print total}'
+echo "Livre: " 
+df |grep /  | awk '{total += $4};END {total/=1048576} ;  END {print total}'
+
+echo "== Memória RAM =="
+echo "Ocupado: " 
+free -h | grep Mem | awk '{print $3}'
+echo "Livre: " 
+free -h | grep Mem | awk '{print $4}'
+
+echo "== Downloads =="
+ls ~/Downloads/
+
+echo "== Home =="
+echo "Videos: " 
+ls -R ~ |grep .mp4 | wc -l
+echo "Imagens: " 
+ls -R ~ |grep .jpg | wc -l
+echo "Músicas: " 
+ls -R ~ |grep .mp3 | wc -l
